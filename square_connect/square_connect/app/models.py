adf51000fe9b29e5d830fc4c2054fabedf2fc34c
Nick Chapman <nick@thecorp.org>
2015-10-16 18:47:16 -0400

    Sarted thinking about what models we need

--- square_connect/square_connect/app/models.py
+++ square_connect/square_connect/app/models.py
@@ -4,3 +4,16 @@ Definition of models.
 
 from django.db import models
 
+class SpoilageReport(models.Model):
+    # TODO
+    # This contains all of the SpoilageItems for a given day
+    pass
+
+class SpoilageItem(models.Model):
+    # TODO
+    # Need to figure out what this needs
+    # I think it needs:
+    #   - its own id
+    #   - Item/product id
+    #   - quantity, ie the number of this item that were spoiled on a given day
+    pass
