61ad86a1511bfc8d9ba4158b9fb3c11c88400450
Max <max@thecorp.org>
2015-10-21 21:34:37 -0500

    Put the categories for the Spoilage item models

--- square_connect/square_connect/app/models.py
+++ square_connect/square_connect/app/models.py
@@ -16,4 +16,20 @@ class SpoilageItem(models.Model):
     #   - its own id
     #   - Item/product id
     #   - quantity, ie the number of this item that were spoiled on a given day
+
+    # For example bagels or sandwiches
+    category = models.CharField(max_length=50)
+
+    # For example chocolate or cinnamon raison
+    subcategory = models.CharField(max_length=50)
+
+    # In case we need to be more specific 
+    sku = models.CharField(max_length=50)
+
+    # Price
+    price = models.IntegerField()
+
+    # Quantity
+    quantity = models.IntegerField()
+
     pass
