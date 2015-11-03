6f49cc893b01ece5683108cc2cbf6134ae617a2f
Max <max@thecorp.org>
2015-11-02 21:29:54 -0500

    HOW-22: made a get report for the spoilage report

--- square_connect/square_connect/app/models.py
+++ square_connect/square_connect/app/models.py
@@ -7,6 +7,28 @@ from django.db import models
 class SpoilageReport(models.Model):
     # TODO
     # This contains all of the SpoilageItems for a given day
+
+    # Gets the spoilage items from a Json report
+
+    spoiledList = []
+
+    def getReport(self, report):
+
+   
+
+        for item in range(0, len(report)):
+            if report[item]['itemizations'][0]['discounts'][0]['name'] == 'Spoilage':
+
+                spoiledItem = SpoilageItem()
+                
+                spoiledItem.name = report[item]['itemizations'][0]['name']
+                spoiledItem.id = report[item]['itemizations'][0]['item_detail']['item_id']
+                spoiledItem.sku = report[item]['itemizations'][0]['item_detail']['sku']
+                spoiledItem.price = report[item]['itemizations'][0]['single_quantity_money']
+
+                self.spoiledList.append(spoiledItem)
+
+
     pass
 
 class SpoilageItem(models.Model):
@@ -17,11 +39,12 @@ class SpoilageItem(models.Model):
     #   - Item/product id
     #   - quantity, ie the number of this item that were spoiled on a given day
 
-    # For example bagels or sandwiches
-    category = models.CharField(max_length=50)
+    # Name of 
+    name = models.CharField(max_length=50)
 
-    # For example chocolate or cinnamon raison
-    subcategory = models.CharField(max_length=50)
+    # Unique own id
+    
+    id = models.CharField(max_length=50)
 
     # In case we need to be more specific 
     sku = models.CharField(max_length=50)
@@ -33,3 +56,5 @@ class SpoilageItem(models.Model):
     quantity = models.IntegerField()
 
     pass
+
+
