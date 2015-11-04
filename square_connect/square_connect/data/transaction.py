bf2a20cac023b5345e02bd645153f2ffe696f30c
Nick Chapman <nick@thecorp.org>
2015-11-04 04:25:27 +0000

    HOW-23: Added documentation and finished some lingering TODOs

--- square_connect/square_connect/data/transaction.py
+++ square_connect/square_connect/data/transaction.py
@@ -13,7 +13,9 @@ def format_money(cents):
     return locale.currency(cents / 100.0)
 
 class SquareRequest:
+    """ Base class for communicating with Square. Includes the basic connection information. """
     def __init__(self, *args, **kwargs):
+        """ Constructor for the base class. Takes no parameters. """
         # Shared for all requests
         self.base_path = "https://connect.squareup.com"
         # The access token can be found from our Square account's app manager dashboard
@@ -32,28 +34,44 @@ class SquareRequest:
         self.response_data = None
         self.response_json = None
 
-    def add_parameter(self, key, value):
-        # TODO: Check to see if the parameter is already set
+    def add_parameter(self, key, value, force=False):
+        """ Adds a parameter, value pair to the request
+        @param key The parameter name
+        @param value The value for the specified parameter
+        @param force If the paramter is set you can change it using the force flag
+        @throw KeyError Thrown when someone tries to set an already set parameter without forcing
+        """
+        if key in self.parameters and not force:
+            error_message = "Parameter \"" + key + "\" is already set, use the force flag to overwrite"
+            raise KeyError(error_message)
         self.parameters[key] = value
 
-    def add_header(self, key, value):
-        # TODO: Check to see if header exists
-        #       Add a force flag so that it will overwrite the default headers
+    def add_header(self, key, value, force=False):
+        """ Adds a header, value pair to the request
+        @param key The header name
+        @param value The value for the specified header
+        @param force If the header is set you can change it using the force flag
+        @throw KeyError Thrown when someone tries to set an already set header without forcing
+        """
+        if key in self.headers and not force:
+            error_message = "Header \"" + key + "\" is already set, use the force flag to overwrite"
+            raise KeyError(error_message)
         self.headers[key] = value
 
     def build_full_path(self):
+        """ Assembles the full request path """
         parameter_values = urllib.parse.urlencode(self.parameters)
         self.full_path = self.base_path + self.request_path + "?" + parameter_values
 
     def create_request(self):
-        # TODO
-        # First update the full path
+        """ Generates a request """
         self.build_full_path()
         self.request = urllib.request.Request(self.full_path)
         for key, value in self.headers.items():
             self.request.add_header(key, value)
 
     def send_request(self):
+        """ Sends an already generated request to Square """
         if self.request is None:
             raise ReferenceError("Attempted to send request without creating one first")
         page_data = urllib.request.urlopen(self.request)
@@ -62,12 +80,17 @@ class SquareRequest:
         self.response_json = json.loads(self.response_data)
 
 class LocationsRequest(SquareRequest):
+    """ Gets our storefront merchant IDs """
     def __init__(self, *args, **kwargs):
+        """ Constructor, takes no parameters """
         super().__init__(args, kwargs)
         
         self.request_path = "/v1/me/locations"
 
     def get_merchant_ids(self):
+        """ Processes the square response data and returns the merchant IDs 
+        @returns A dictionary of the store merchant IDs with the store names as the keys
+        """
         if self.response_json is None:
             raise ReferenceError("Attempted to access response data without first running a request")
         locations = {}
@@ -79,11 +102,17 @@ class LocationsRequest(SquareRequest):
         return locations
 
     def auto(self):
+        """ Builds a request, sends the request, and returns the merchant IDs 
+        
+        Takes care of almost everything for you
+        @returns A dictionary of the store merchant IDs with the store names as the keys
+        """
         self.create_request()
         self.send_request()
         return self.get_merchant_ids()
 
 class PaymentRequest(SquareRequest):
+    """  """
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
 
