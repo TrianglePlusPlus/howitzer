3d1a926dd068e71958fc7749bbc1d37921f0ef2a
Nick Chapman <nick@thecorp.org>
2015-12-04 18:05:00 -0500

    Checking in so I can get up to date with master.

--- square_connect/square_connect/spoilage_report/models.py
+++ square_connect/square_connect/spoilage_report/models.py
@@ -1,39 +1,36 @@
 ﻿from django.db import models
 # Importing models from other apps
 from square_connect.app.models import Service
+from square_connect.data.transaction import 
 
 class SpoilageReport(models.Model):
     # TODO
     date = models.DateField()
     service = models.ForeignKey("Service")
-    # Gets the spoilage items from a Json report
-    def getReport(self, report):
-
-        # Runs through report to populate a list of spoiled items with relevant fields
+    
+    def add_items_from_json_data(self, json_data):
         for item in report:
             if item['itemizations'][0]['discounts'][0]['name'] == 'Spoilage':
-
-                spoiledItem = SpoilageItem()
+                spoiled_item = SpoilageItem.objects.create()
+                spoiled_item.name = report[item]['itemizations'][0]['name']
+                spoiled_item.id = report[item]['itemizations'][0]['item_detail']['item_id']
+                spoiled_item.sku = report[item]['itemizations'][0]['item_detail']['sku']
+                spoiled_item.price = report[item]['itemizations'][0]['single_quantity_money']
+                spoiled_item.save()
+    
+    def get_report(self, report):
+        pass
                 
-                spoiledItem.name = report[item]['itemizations'][0]['name']
-                spoiledItem.id = report[item]['itemizations'][0]['item_detail']['item_id']
-                spoiledItem.sku = report[item]['itemizations'][0]['item_detail']['sku']
-                spoiledItem.price = report[item]['itemizations'][0]['single_quantity_money']
-
-                self.spoiledList.append(spoiledItem)
-
-    def find_items(self):
+    def find_associated_items(self):
         spoilage_items = SpoilageItem.objects.filter(pk=self.pk)
+        # TODO
 
 class SpoilageItem(models.Model):
     # TODO
-<<<<<<< HEAD
-    name = models.CharField(max_length=50) # Max length 50
-    sku = models.CharField(max_length=12) # Max length 12
-=======
+    name = models.CharField(max_length=50) 
+    sku = models.CharField(max_length=12) 
     name = models.CharField(max_length=50)
     sku = models.CharField(max_length=12)
->>>>>>> parent of 67c3cb2... Shit is getting out of control
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.IntegerField()
     # The report is the SpoilageReport which the item belongs to
