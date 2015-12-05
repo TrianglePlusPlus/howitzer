569d5e6cfb91246d0ee08b8f08794bff2e5301d6
Nick Chapman <nick@thecorp.org>
2015-12-04 20:34:29 -0500

    MySQL seems to be fully configured to be auto setup every time

--- vagrant_bootstrap/bootstrap.sh
+++ vagrant_bootstrap/bootstrap.sh
@@ -4,28 +4,29 @@
 apt-get update
 
 # Installing apache2
-echo "INSTALLING apache2..."
+echo "************* INSTALLING APACHE2 *************"
 apt-get install -y apache2
 
 # Installing Nginx
 apt-get install -y nginx
 
 # Installing helpers for PHP install
-echo "UPDATING PHP REPOSITORY..."
+echo "************* UPDATING PHP REPOSITORY *************"
 apt-get install -y python-software-properties build-essential
-add-apt-repository -y ppa:ondrej/php5
+add-apt-repository -y ppa:ondrej/php5 # This is more up to date than the ubuntu one
 apt-get update	
 
 # Installing PHP for phpmyadmin
-echo "INSTALLING PHP..."
+echo "************* INSTALLING PHP *************"
 apt-get install -y php5-common php5-dev php5-cli php5-fpm
-echo "INSTALLING EXTENSIONS..."
+echo "************* INSTALLING EXTENSIONS *************"
 apt-get install -y curl php5-curl php5-gd php5-mcrypt php5-mysql
 
 # Installing debconf-utils for MySQL install
+echo "************* INSTALLING DEBCONF-UTILS *************"
 apt-get install -y debconf-utils
 
-# THIS DOESN'T WORK CORRECTLY, TODO
+# TODO
 # Installing PHPMyAdmin
 #echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
 #echo "phpmyadmin phpmyadmin/app-password-confirm password bkOpQa0g7d" | debconf-set-selections
@@ -43,18 +44,33 @@ if ! [ -L /var/www ]; then
 fi
 
 # Installing pip
+echo "************* INSTALLING PIP3 *************"
 apt-get -y install python3-pip
 
-# Setting up the virtualenv with Django
+# Setting up the virtualenv directory
 if [ ! -d /vagrant/square_connect/square_connect/virt_env ]; then
-	pip3 install virtualenv
 	mkdir /vagrant/square_connect/square_connect/virt_env
+fi
+
+# Installing virtualenv if necessary
+if ! type virtualenv >/dev/null 2>&1; then
+	echo "************* INSTALLING VIRTUALENV *************"
+	pip3 install virtualenv
 	virtualenv /vagrant/square_connect/square_connect/virt_env --always-copy
-	source /vagrant/square_connect/square_connect/virt_env/bin/activate
+fi
+
+# Activate the virtualenv for the next couple installs
+source /vagrant/square_connect/square_connect/virt_env/bin/activate
+
+# Install django
+if ! type django-admin >/dev/null 2>&1; then
+	echo "************* INSTALLING DJANGO *************"
 	pip3 install django
-	
-	# Installing MySQL
-	echo "CONFIGURING MYSQL INSTALL..."
+fi
+
+# Install MySQL
+if ! type mysql >/dev/null 2>&1; then
+	echo "************* CONFIGURING MYSQL INSTALL *************"
 	echo "mysql-server mysql-server/root_password password S89ydkH6rnwh9GNm2Um" | debconf-set-selections
 	echo "mysql-server mysql-server/root_password_again password S89ydkH6rnwh9GNm2Um" | debconf-set-selections
 	echo "INSTALLING MYSQL..."
@@ -62,21 +78,30 @@ if [ ! -d /vagrant/square_connect/square_connect/virt_env ]; then
 
 	
 	# Installing mysqlclient
-	echo "INSTALLING MYSQLCLIENT HEADERS..."
-	apt-get install -y libmysqlclient-dev python3-dev
-	echo "INSTALLING PYTHON 3 DEV..."
-	echo "INSTALLING MYSQLCLIENT..."
+	echo "************* INSTALLING MYSQLCLIENT HEADERS *************"
+	apt-get install -y libmysqlclient-dev
+	echo "************* INSTALLING PYTHON 3 DEV *************"
+	apt-get install -y python3-dev
+	echo "************* INSTALLING MYSQLCLIENT *************"
 	pip3 install mysqlclient
-	deactivate
+
+	# Run the MySQL configuration script
+	chmod +x /vagrant/vagrant_bootstrap/mysql_configure.sh
+	/vagrant/vagrant_bootstrap/mysql_configure.sh
 fi
 
+# Virtualenv based installed finished, deactive
+deactivate
 
+# Enable apache_configure.sh to run 
 chmod +x /vagrant/vagrant_bootstrap/apache_configure.sh
 	
-# Configure apache so it doesn't freak out
+# Configure apache
+echo "************* CONFIGURING APACHE *************"
 /vagrant/vagrant_bootstrap/apache_configure.sh
 
 # Install git for convenience
+echo "************* INSTALLING GIT *************"
 apt-get -y install git
 
 
