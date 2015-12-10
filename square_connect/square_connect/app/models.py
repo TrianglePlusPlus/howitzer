5d12dcfa194b68670092748ed6faf4e13ff1969c
Nick Chapman <nick@thecorp.org>
2015-12-09 21:59:33 -0500

    HOW-21: Welp, everyone is at Gala and this shift is over. Didn't go, cuz ya know, poverty.

--- square_connect/square_connect/app/models.py
+++ square_connect/square_connect/app/models.py
@@ -7,12 +7,27 @@ class Service(models.Model):
 
     @staticmethod
     def regenerate_services():
-        # TODO
+        """ Populates the database with services and merchant ids
+        If a service is not found in the database it is entered
+        If a merchant id was changed on Square's side then it is updated
+        """
         locations = LocationsRequest.auto()
-        active_locations = Service.objects.all().values('name')
-        names = []
-        for entry in active_locations:
-            names.append(entry["name"])
+        active_locations = Service.objects.all()
+        # Add locations based on name
         for location, store_merchant_id in locations.items():
-            if location not in names:
+            location_found = False
+            for entry in active_locations:
+                if entry.name == location:
+                    location_found = True
+            if not location_found:
                 Service.objects.create(name=location, merchant_id=store_merchant_id)
+        # Update merchant ids if they are outdated
+        for location, store_merchant_id in locations.items():
+            merchant_id_found = False
+            for entry in active_locations:
+                if entry.merchant_id == store_merchant_id:
+                    merchant_id_found = True
+            if not merchant_id_found:
+                storefront = Service.objects.get(name=location)
+                storefront.merchant_id = store_merchant_id
+                storefront.save()
