1d032836d5ca62d435c6c5dcc6651669bd5ce53c
Max <max@thecorp.org>
2016-03-16 20:37:31 -0400

    HOW-31: Got the REGEX working for url parsing. Services still not working

--- square_connect/square_connect/spoilage_report/views.py
+++ square_connect/square_connect/spoilage_report/views.py
@@ -32,3 +32,50 @@ def spoilage_report(request):
             'year':'Remember never give up.',
         })
     )
+	
+def spoilage_date(request, service_location, year, month, day):
+	"""Renders the reports for a given date"""
+	
+	assert isinstance(request, HttpRequest)
+	
+	date = year + '-' + month + '-' + day
+	service = service_location
+	
+	report = SpoilageReport.objects.filter(date=date, service__nam=service)
+	if report.count() > 0:
+            report = SpoilageReport.objects.get(date=date, service__name=service)
+	return render(
+        request,
+        'spoilage_report/spoilage_report.html',
+        context_instance = RequestContext(request,
+        {
+            'report':report,
+            'title':'Report Viewer',
+            'year':'Remember never give up.',
+        })
+    )
+	
+	
+def test_spoilage_report(request):
+    assert isinstance(request, HttpRequest)
+    # Check if they are searching for a report
+    if request.POST.get('date', False):
+        # They are searching for a report
+        date = request.POST.get('date', None)
+        date = datetime.datetime.strptime(date, "%m/%d/%Y").date()
+        service = request.POST.get('service', None)
+        report = SpoilageReport.objects.filter(date=date, service__name=service)
+        if report.count() > 0:
+            report = SpoilageReport.objects.get(date=date, service__name=service)
+    else:
+        report = None
+    return render(
+        request,
+        'spoilage_report/test_spoilage_report.html',
+        context_instance = RequestContext(request,
+        {
+            'report':report,
+            'title':'Report Viewer',
+            'year':'Test: Remember never give up.',
+        })
+    )
