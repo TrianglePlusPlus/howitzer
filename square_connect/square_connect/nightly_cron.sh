060e87b4b3d758efe3aa209036d8df521ca41f14
Peter <peter.johnston.xc@gmail.com>
2016-09-23 19:46:12 +0000

    correct cron script

--- square_connect/square_connect/nightly_cron.sh
+++ square_connect/square_connect/nightly_cron.sh
@@ -1,9 +1,9 @@
-# USE THIS FILE TO SCHEDULE TASKS THAT RUN EVERY NIGHT
+# USE THIS FILE TO SCHEDULE TASKS THAT RUN EVERY DAY
 
-# SPOILAGE DATA PULLS
+# SPOILAGE REPORT EMAILS
 # Start the virtual environment
-. /vagrant/square_connect/square_connect/virt_env/bin/activate
-# Run the pull command
-python /vagrant/square_connect/square_connect/manage.py send_emails
+. /home/thecorp/django/howitzer/square_connect/square_connect/virt_env/bin/activate
+# Run the email command
+python /home/thecorp/django/howitzer/square_connect/square_connect/manage.py send_emails
 # Close the virtual environment
 deactivate
