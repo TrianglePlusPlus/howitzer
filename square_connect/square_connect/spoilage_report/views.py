de018a1a869de298d040db0ca2e67e70562a304c
Peter <peter.johnston.xc@gmail.com>
2016-10-21 16:25:08 -0400

    setup csv export with sample data

--- square_connect/square_connect/spoilage_report/views.py
+++ square_connect/square_connect/spoilage_report/views.py
@@ -8,7 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.serializers.json import DjangoJSONEncoder
 from django.contrib.auth.decorators import login_required
 
-import json
+import json, csv
 
 @login_required
 def spoilage_report(request):
@@ -29,6 +29,7 @@ def spoilage_report(request):
         }
     )
 
+@login_required
 def spoilage_report_date(request, service_location, start_year, start_month, start_day, end_year, end_month, end_day):
     """Renders the reports for a given date. Takes in URL regex as parameters. This should only be called from urls.py
     @param request: Takes a request for spoilage
@@ -59,8 +60,6 @@ def spoilage_report_date(request, service_location, start_year, start_month, sta
         }
     )
 
-@csrf_exempt
-# @require_POST ?
 def request_report(request):
     """Requests the spoilage data.
     request.POST dictionary keys:
@@ -101,6 +100,60 @@ def request_report(request):
             json.dumps(return_data, cls=DjangoJSONEncoder),
             content_type="application/json"
         )
+    else:
+        return HttpResponse(
+            json.dumps({"POST method failed": "we have no data for you"}),
+            content_type="application/json"
+        )
+
+def export_csv(request):
+    """Exports the report as a .CSV.
+    request.POST dictionary keys:
+        start_date
+        end_date
+        service
+	@param request: Takes in a request query to return a CSV file of filtered spoilage data. Queries must have a date range as well as service
+	@returns filtered spoilage data based on a date in .CSV format
+	"""
+    if request.method == "POST":
+        return_data = {}
+        sum_total = 0
+
+        # Check if they are searching for a report
+        if request.POST.get('start_date', False) and request.POST.get('end_date', False):
+            # They are searching for a report
+            start_date = request.POST.get('start_date', None)
+            end_date = request.POST.get('end_date', None)
+            start_date = datetime.strptime(start_date, "%m/%d/%Y").date()
+            end_date = datetime.strptime(end_date, "%m/%d/%Y").date()
+            service = request.POST.get('service', None)
+            reports = SpoilageReport.search_reports(start_date, end_date, service)
+            reports_list = []
+            if reports.count() > 0:
+                for report in reports:
+                    sum_total += report.get_total
+                    reports_list.append(report.dictionary_form())
+                return_data = {
+                    "reports": reports_list,
+                    "sum_total": sum_total
+                }
+        else:
+            reports = None # is this really necessary?
+
+        # Create the HttpResponse object with the appropriate CSV header.
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="export.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+        writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+        for report in reports:
+            # iterate over reports
+            writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+            writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+
+        return response
+
     else:
         return HttpResponse(
             json.dumps({"POST method failed": "we have no data for you"}),
