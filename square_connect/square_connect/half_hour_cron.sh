027f577b799ce4e46450bc90519e48187f902c0b
Max <maxwkim@outlook.com>
2016-10-24 20:47:57 -0400

    HOW-6: Updated half hour chron

--- square_connect/square_connect/half_hour_cron.sh
+++ square_connect/square_connect/half_hour_cron.sh
@@ -1,9 +1,9 @@
 # USE THIS FILE TO SCHEDULE TASKS THAT RUN EVERY 30 MINUTES
 
-# SPOILAGE DATA PULLS
+# REPORT DATA PULLS
 # Start the virtual environment
 . /vagrant/square_connect/square_connect/virt_env/bin/activate
 # Run the pull command
-python /vagrant/square_connect/square_connect/manage.py get_recent_spoilage_transactions
+python /vagrant/square_connect/square_connect/manage.py get_recent_transactions All
 # Close the virtual environment
 deactivate
