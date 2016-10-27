a58d0b24087f51b8703177f309c3726fe1bfee4a
Casey Knerr <caseyknerr@gmail.com>
2016-10-26 21:55:22 -0400

    AJAX FRONTEND

--- square_connect/square_connect/mailer/views.py
+++ square_connect/square_connect/mailer/views.py
@@ -57,37 +57,52 @@ def mailer_admin(request, retcode=None):
         'mailer/mailer_admin.html'
     )
 
+# add_emp function
 @csrf_exempt
 def add_emp(request):
-
+    
+    # if the request is a POST request
     if request.method == "POST":
 
+        # get data from the POST request
         mailing_list_num = request.POST.get('mailing_list', None)
-        mailing_list = MailingList.objects.get(id=mailing_list_num)
-        service = mailing_list.service
         first_name = request.POST.get('first_name', None)
         last_name = request.POST.get('last_name', None)
         email = request.POST.get('email', None)
-        person = Person.objects.create_person(mailing_list,first_name,last_name,email)
-        person.save()
 
-        response_data = {}
-        response_data['result'] = 'Successful Submit!'
-        response_data['person_id'] = person.id
-        response_data['person_first_name'] = person.first_name
-        response_data['person_last_name'] = person.last_name
-        response_data['person_email'] = person.email
-        response_data['person_service'] = service.name
-        response_data['person_service_id'] = service.merchant_id
+        # get additional information from the request data
+        mailing_list = MailingList.objects.get(id=mailing_list_num)
+        service = mailing_list.service
+        
+        try:
+            # send an email to the added individual
+            send_mail(
+                "Mailing List Notification",
+                "Hello " + first_name + " " + last_name + "! You were added to the " + service.name + " mailing list.",
+                "reports@thecorp.org",
+                [email],
+                fail_silently = False,
+            )
 
-        send_mail(
-            "Mailing List Notification",
-            "Hello " + first_name + " " + last_name + "! You were added to the " + service.name + " mailing list.",
-            "reports@thecorp.org",
-            [email],
-            fail_silently = False,
-        )
+            # create a new Person object with the information
+            person = Person.objects.create_person(mailing_list,first_name,last_name,email)
+            person.save()
 
+            # assemble a dictionary of information with which to respond to the POST request
+            response_data = {}
+            response_data['result'] = 'Successful Submission!'
+            response_data['person_id'] = person.id
+            response_data['person_first_name'] = person.first_name
+            response_data['person_last_name'] = person.last_name
+            response_data['person_email'] = person.email
+            response_data['person_service'] = service.name
+            response_data['person_service_id'] = service.merchant_id
+
+        except:
+           response_data = {}
+           response_data['result'] = 'fail'
+
+        # return the response data
         return HttpResponse(
             json.dumps(response_data),
             content_type = "application/json"
@@ -99,15 +114,20 @@ def add_emp(request):
             content_type = "application/json"
         )
 
+# delete_emp function
 @csrf_exempt
 def delete_emp(request):
     
+    # if the request is a POST request
     if request.method == "POST":  
         
+        # get data from the POST request
         person_id = request.POST.get('person', None)
-        print(person_id)
+         
+        # get additional information from the request data
         person = Person.objects.get(id=person_id)
         
+        # assemble a dictionary of information with thich to respond to the POST request
         response_data = {}
         response_data['result'] = 'Successful Delete!'
         response_data['person_id'] = person.id
@@ -117,9 +137,10 @@ def delete_emp(request):
         response_data['person_service'] = person.mailing_list.service.name
         response_data['person_service_id'] = person.mailing_list.service.merchant_id
 
-
+        # delete an old Person object with the information
         person.delete()
 
+        # resturn the response data
         return HttpResponse(
             json.dumps(response_data),
             content_type="application/json"
