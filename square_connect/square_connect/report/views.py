9864a64494b2e627923a50b2e03381a3a3e862e4
sallymatson <sally.matson@gmail.com>
2016-11-30 20:30:55 -0500

    moved over to reports etc

--- square_connect/square_connect/report/views.py
+++ square_connect/square_connect/report/views.py
@@ -169,3 +169,45 @@ def export_csv(request):
             json.dumps({"POST method failed": "we have no data for you"}),
             content_type="application/json"
         )
+
+
+def generate_graph(request):
+    if request.method == "POST":
+
+        return_data = {}
+        sum_total = 0
+        
+        if request.POST.get('start_date', False) and request.POST.get('end_date', False):
+            # They are searching for a report
+            start_date = request.POST.get('start_date', None)
+            end_date = request.POST.get('end_date', None)
+            start_date = datetime.strptime(start_date, "%m/%d/%Y").date()
+            end_date = datetime.strptime(end_date, "%m/%d/%Y").date()
+            service = request.POST.get('service', None)
+            reports = Report.search_reports(start_date, end_date, service)
+            reports_list = []
+            if reports.count() > 0:
+                for report in reports:
+                    sum_total += report.get_total
+                    reports_list.append(report.dictionary_form())
+                return_data = {
+                    "reports": reports_list,
+                    "sum_total": sum_total
+                }
+        else:
+            reports = None # is this really necessary?
+
+        return HttpResponse(
+            json.dumps(return_data, cls=DjangoJSONEncoder),
+            content_type="application/json"
+        )
+        return request_report(request)
+        return render(
+            request,
+            'spoilage_report/generate_graph.html',
+            {
+                'report_data':json,
+                'title':'Report Viewer',
+                'year':'Remember never give up.',
+            }
+        )
