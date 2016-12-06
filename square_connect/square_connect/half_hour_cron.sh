285d4c7c3727fd5c8d1ef6cdb5ab1bfe26dce153
Peter <peter.johnston.xc@gmail.com>
2016-12-06 00:11:16 -0500

    fix half_hour_cron for generalized reports

--- square_connect/square_connect/half_hour_cron.sh
+++ square_connect/square_connect/half_hour_cron.sh
@@ -5,7 +5,7 @@
 # Start the virtual environment
 source /home/thecorp/django/howitzer/square_connect/square_connect/virt_env/bin/activate
 # Run the pull command
-python /home/thecorp/django/howitzer/square_connect/square_connect/manage.py get_recent_spoilage_transactions
+python /home/thecorp/django/howitzer/square_connect/square_connect/manage.py get_recent_transactions All
 python /vagrant/square_connect/square_connect/manage.py get_recent_transactions All
 # Close the virtual environment
 deactivate
