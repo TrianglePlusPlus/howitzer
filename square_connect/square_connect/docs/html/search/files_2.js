39390cca194bab85c02b2622f7f9063d3ff3d1fb
peterthejohnston <peter.johnston.xc@gmail.com>
2017-02-12 15:47:50 -0500

    move docs to top level directory

--- square_connect/square_connect/docs/html/search/files_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['main_2emd',['main.md',['../main_8md.html',1,'']]],
-  ['models_2epy',['models.py',['../mailer_2models_8py.html',1,'']]],
-  ['models_2epy',['models.py',['../report_2models_8py.html',1,'']]],
-  ['models_2epy',['models.py',['../app_2models_8py.html',1,'']]]
-];
